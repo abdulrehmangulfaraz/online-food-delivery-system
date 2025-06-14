788056b9cc63c54c231e7ee5025dc73c38cfbd2b
Abdulrehman <157941389+abdulrehmangulfaraz@users.noreply.github.com>
2025-06-14 10:24:59 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Sherii',
+  password: '',
   database: 'online_food_db',
   port: 3306
 });
